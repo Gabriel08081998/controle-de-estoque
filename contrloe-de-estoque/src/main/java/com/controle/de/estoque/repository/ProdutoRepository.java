2d4d793916226f490d5527111109215e6d394a3a
gabriel <gabrieloliveira098@outlook.com>
2023-11-15 20:24:08 -0300

    Esse projeto é um sistema de gerenciamento de estoque e vendas desenvolvido em Java usando o framework Spring Boot. Ele inclui funcionalidades para cadastrar e consultar produtos, realizar vendas, e atualizar o estoque dos produtos. O sistema utiliza um banco de dados MySQL para armazenar informações sobre produtos e vendas, e as operações são manipuladas por meio de controladores, serviços e repositórios, seguindo boas práticas de desenvolvimento. O código também inclui DTOs para transferência de dados e utiliza anotações do Hibernate para mapear entidades do banco de dados. O projeto é iniciado por meio da classe GerenciadorEstoqueApplication

--- contrloe-de-estoque/src/main/java/com/controle/de/estoque/repository/ProdutoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.controle.de.estoque.repository;
-
-import com.controle.de.estoque.model.Produto;
-import org.springframework.data.repository.CrudRepository;
-
-
-public interface ProdutoRepository extends CrudRepository<Produto,Long> {
-
-}
--- contrloe-de-estoque/src/main/java/com/controle/de/estoque/repository/VendaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.controle.de.estoque.repository;
-
-import com.controle.de.estoque.model.Venda;
-import org.springframework.data.repository.CrudRepository;
-
-public interface VendaRepository extends CrudRepository<Venda,Long> {
-
-}
